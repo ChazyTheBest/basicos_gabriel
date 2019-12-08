7fb5cd4417f6f703d5a1b924f81fb3eb296de13e
Gabriel Blanca Montes <how2hack@hotmail.es>
2019-12-08 18:15:23 +0100

    method for String data type

--- src/basicos/Main.java
+++ src/basicos/Main.java
@@ -2,8 +2,23 @@ package basicos;
 
 public class Main
 {
+    /**
+     * String non-primitive data type
+     */
+    private static void cadenas()
+    {
+        // Variable declaration
+        String myString;
+
+        // Variable initialization
+        myString = "I am a String";
+
+        // Print variable
+        System.out.printf("Hello: %s.\n", myString);
+    }
+
     public static void main(String[] args)
     {
-
+        cadenas();
     }
 }
